f84d3d573f7ea7c1adc5886c432406acb9d2f810
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-21 15:06:30 -0500

    one classroom test done

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,10 +1,23 @@
 package io.zipcoder;
-
-
+import org.junit.Assert;
 import org.junit.Test;
+import io.zipcoder.Student;
+import io.zipcoder.Classroom;
 
 public class ClassroomTest {
 
-   // @Test
+   @Test
+   public void getStudentsTest(){
+       Student bilbo = new Student("Bilbo","Baggins",new Double[] {100.0});
+       Classroom mordor = new Classroom();
+       mordor.addStudent(bilbo);
+       Student[] definite = new Student[]{bilbo};
+       Student[] maybe = mordor.getStudents();
+       Boolean actual = definite[0] == maybe[0];
+       boolean expected = true;
+       Assert.assertEquals(expected,actual);
+
+
+   }
 
 }
