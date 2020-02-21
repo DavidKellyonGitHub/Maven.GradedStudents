ebc5ba029397221b7735b4ab6b91b5aadbf2e26f
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-21 13:57:44 -0500

    added addStudent and removeStudent

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,16 +1,21 @@
 package io.zipcoder;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
 import java.util.Arrays;
 
 public class Classroom {
     private Student[] students;
 
     public Classroom(int maxNumberOfStudents){
+        this.students = new Student[maxNumberOfStudents];
 
     }
 
     public Classroom(Student[] enrolled){
-
+        for (int i = 0;i < enrolled.length;i++){
+            this.students[i] = enrolled[i];
+        }
     }
 
     public Classroom(){
@@ -28,4 +33,22 @@ public class Classroom {
         }
         return sum/this.students.length;
     }
+
+    public void addStudent(Student student){
+        for (int i = 0;i<this.students.length;i++){
+            if (this.students[i]==null){
+                this.students[i] = student;
+                break;
+            }
+        }
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        for (int i = 0;i<this.students.length;i++){
+            if ((this.students[i].getFirstName() == firstName) && this.students[i].getLastName()== lastName){
+                this.students[i] = null;
+            }
+        }
+    }
+
 }
